5cfa37d692702e799f255e0f6277e6d911dc8d40
Yordanov <iliyanyyordanov@gmail.com>
2025-04-25 22:25:34 +0100

    Update server.js debugging
    Debug as error occurred due to taskRoutes.js not yet completed.

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(cors());
 app.use(express.json());
 
 // Routes
-app.use("/api/tasks", require("./routes/taskRoutes"));
+//app.use("/api/tasks", require("./routes/taskRoutes"));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
