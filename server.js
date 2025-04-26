65265e0337452ec022e059eef8cc05f90737c660
Yordanov <iliyanyyordanov@gmail.com>
2025-04-26 13:28:37 +0100

    Update server.js
    Simplify code and description comments.

--- server.js
+++ server.js
@@ -1,17 +1,20 @@
-const express = require("express");
-const dotenv = require("dotenv");
-const cors = require("cors");
-const connectDB = require("./config/db");
+const express = require('express');
+const cors = require('cors');
+const connectDB = require('./config/db');
+const taskRoutes = require('./routes/taskRoutes')
+require('dotenv').config();
 
-dotenv.config();
+//Connecting to MongoDB
 connectDB();
 
+//API app setting
 const app = express();
-app.use(cors());
 app.use(express.json());
+app.use(cors());
 
-// Routes
-//app.use("/api/tasks", require("./routes/taskRoutes"));
+//API routing handling
+app.use('/api/v1/ctm', taskRoutes);
 
+//Setting port
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
